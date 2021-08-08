132faf5edbbca95c138862f578522817853bcdc3
agyanatma <agyanatma1410@gmail.com>
2021-08-08 14:35:43 +0700

    initial commit

---
+++ src/components/FormikChallenge/FormikChallenge.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FormikChallenge = () => {
+    return (
+        <div>
+            {/* counter dimulai dari 1 */}
+            <div>Formik Challenge (counter: 1)</div>
+        </div>
+    );
+};
+
+export default FormikChallenge;
